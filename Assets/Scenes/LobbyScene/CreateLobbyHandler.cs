b6cae8449f6f415dc59df68bd8b030fa09ce93c4
Daanish B <daanishbawan@outlook.com>
2023-07-29 02:26:52 -0400

    RelayNotDone

--- Assets/Scenes/LobbyScene/CreateLobbyHandler.cs
+++ Assets/Scenes/LobbyScene/CreateLobbyHandler.cs
@@ -3,19 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Services.Core;
 using System;
+using System.Threading.Tasks;
+
+using System.Linq;
+using Unity.Services.Authentication;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Http;
+using Unity.Services.Relay.Models;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport;
+using Unity.Networking.Transport.Relay;
+using NetworkEvent = Unity.Networking.Transport.NetworkEvent;
 
 public class CreateLobbyHandler : MonoBehaviour
 {
     // Connection Configuration
-    const int MaxConnections = 16;
+    const int MaxConnections = 12;
     public string AccessCodeFromRelay;
 
-    public void GetAccessCode()
+    async void AuthenticateHost()
     {
-
+        try
+        {
+            await UnityServices.InitializeAsync();
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            var playerID = AuthenticationService.Instance.PlayerID;
+        }
+        catch(Exception e)
+        {
+            Debug.Log("Failed To Authenticate Player");
+        }
+        Debug.Log("Host Authentication Made");
     }
 
-    public static async Task<RelayServerData> AllocateRelayServerAndFetchJoinCode(int MaxConnections = 16, region)
+    public static async Task<RelayServerData> AllocateRelayServerAndFetchJoinCode(int MaxConnections = 16, string region = null)
     {
         Allocation allocate; 
         string JoinCode;
@@ -30,7 +52,7 @@ public class CreateLobbyHandler : MonoBehaviour
 
         Debug.Log("Allocation Made");
 
-         try
+        try
         {
             createJoinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
         }
@@ -39,5 +61,7 @@ public class CreateLobbyHandler : MonoBehaviour
             Debug.LogError("Relay create join code request failed");
             throw;
         }
+
+        return new RelayServerData(allocate, "dtls");
     }
 }
