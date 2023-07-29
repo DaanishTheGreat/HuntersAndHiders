d2800fa590e355dc0e68f678cc101051acc9cf2f
Daanish B <daanishbawan@outlook.com>
2023-07-29 00:52:17 -0400

    Commit17

--- Assets/Scenes/LobbyScene/CreateLobbyHandler.cs
+++ Assets/Scenes/LobbyScene/CreateLobbyHandler.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Services.Core;
+using System;
 
 public class CreateLobbyHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Connection Configuration
+    const int MaxConnections = 16;
+    public string AccessCodeFromRelay;
+
+    public void GetAccessCode()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public static async Task<RelayServerData> AllocateRelayServerAndFetchJoinCode(int MaxConnections = 16, region)
     {
-        
+        Allocation allocate; 
+        string JoinCode;
+        try
+        {
+            allocate = await RelayService.Instance.CreateAllocationAsync(maxConnections, region);
+        }
+        catch(Exception e)
+        {
+            Debug.Log("Failed To Create Allocation(Maybe Unity Relays is Down?)");
+        }
+
+        Debug.Log("Allocation Made");
+
+         try
+        {
+            createJoinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+        }
+        catch
+        {
+            Debug.LogError("Relay create join code request failed");
+            throw;
+        }
     }
 }
